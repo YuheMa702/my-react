76a8fa3c8f8dedac3d766f99ec6bf297f03b258c
Yuhe Ma <mayuhe2002doubleflowers@gmail.com>
2024-02-18 16:24:06 -0500

    Added header to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Header() {
+  return <h1>Hello World!</h1>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
